30cd8e24b9ab00ed86a40a01dc5ae764db69d677
Patrick Smith <patrick@nerdgeekhero.com>
2023-03-02 05:02:36 -0800

    fixed formatting of output to be compatible with https://github.com/Hatberg/ashitav4-attendance

--- attendance.lua
+++ attendance.lua
@@ -1,7 +1,7 @@
 _addon.name = 'attendance'
-_addon.author = 'ekrividus'
+_addon.author = 'ainais'
 _addon.version = '0.0.1'
-_addon.commands = {'attendance', 'attend'}
+_addon.commands = {'attendance', 'att'}
 
 require('logger')
 require('xml')
@@ -13,15 +13,10 @@ config = require('config')
 
 local is_ready = false
 local known_players = T{}
-local event = T{}
-local event_names = T{"Dyna - D","Dyna - D - Push","Omen","Vagary","Delve","Social",}
-local event_types = T{"Farm","Wave 1 Boss","Wave 2 Boss","Wave 3 Boss","W3 Group","W3 Windurst","W3 Bastok","W3 Sandoria","W3 Jeuno","Fu","Gin","Kei","Kin","Kyou","Ou","Mid-Boss",}
 local ignore_members = T{}
 local show_debug_once = false
 
 local defaults = T{
-    event_names = T{"Dyna - D", "Omen"},
-    event_types = T{"Wave 1 Boss", "Wave 2 Boss", "Wave 3 Boss"},
     ignore_members = T{},
 }
 local settings = config.load(defaults)
@@ -47,85 +42,26 @@ function get_player_data()
     local party = windower.ffxi.get_party()
     local current_members = T{}
     local id = 0
+    local zone = 0
 
     for k, v in pairs (party) do
         if (type(v) == "table") then
             if (v.mob and not v.is_npc) then
                 id = v.mob.id
-                current_members[id] = {name = v.mob.name}
+                current_members[id] = {name = v.name, zone = res.zones[v.zone].name}
                 if (known_players[id]) then
                     current_members[id].main_job = res.jobs[known_players[id].main_job].ens
                     current_members[id].main_job_lvl = known_players[id].main_job_lvl
                     current_members[id].sub_job = res.jobs[known_players[id].sub_job].ens
                     current_members[id].sub_job_lvl = known_players[id].sub_job_lvl
                 end
+            elseif (not v.is_npc) then
+                current_members[v.name] = {name = v.name, zone = res.zones[v.zone].name}
             end
         end
     end
     return current_members
 end
-
-function save_report_xml()
-    if (not is_ready) then
-        windower.add_to_chat(207, "Just a little longer please.")
-        return
-    end
-    local date = os.date('*t')
-    local time = os.date('%H%M%S')
-    local name = windower.ffxi.get_player().name
-    local timestamp = ('%.4u.%.2u.%.2u.%.2u'):format(date.year, date.month, date.day, time)
-    local data = T{}
-    local current_members = get_player_data()
-    local report = ""
-    local filename = (event.name ~= '' and (event.name ~= 'Unknown' and event.name) or 'attendance')..('_%.4u.%.2u.%.2u_%.2u.xml'):format(date.year, date.month, date.day, time)
-
-    local file = files.new('/export/'..filename)
-    if not file:exists() then
-        file:create()
-    end
-    file:append('<?xml version="1.1" ?>\n')
-    data.event = T{}
-    data.event.members = T{}
-    for k,v in pairs(current_members) do
-        if (not ignore_members:contains(v.name)) then
-            data.event.members[v.name] = T{}
-            data.event.members[v.name].player_name = v.name
-            data.event.members[v.name].main_job = 'Unknown'
-            data.event.members[v.name].main_job_lvl = '0'
-            data.event.members[v.name].sub_job = 'Unknown'
-            data.event.members[v.name].sub_job_lvl = '0'
-            data.event.members[v.name].main_job = tostring(v.main_job)
-            data.event.members[v.name].main_job_lvl = tostring(v.main_job_lvl)
-            data.event.members[v.name].sub_job = tostring(v.sub_job)
-            data.event.members[v.name].sub_job_lvl = tostring(v.sub_job_lvl)
-            data.event.members[v.name].early = "false" -- Early
-            data.event.members[v.name].ontime = "false" -- Ontime
-            data.event.members[v.name].late = "false" -- Late
-            data.event.members[v.name].role = 'Unknown'
-            if (T{"PLD","NIN","RUN"}:contains(v.main_job)) then
-                data.event.members[v.name].role = 'Tank'
-            elseif (T{"WHM","SCH"}:contains(v.main_job)) then
-                data.event.members[v.name].role = 'Healer'
-            elseif (T{"RDM","BRD","COR","GEO"}:contains(v.main_job)) then
-                data.event.members[v.name].role = 'Support'
-            elseif (T{"WAR","MNK","THF","DRK","BST","RNG","SAM","DRG","BLU","PUP","DNC"}:contains(v.main_job)) then
-                data.event.members[v.name].role = 'Physical DD'
-            elseif (T{"BLM","SMN"}:contains(v.main_job)) then
-                data.event.members[v.name].role = 'Magical DD'
-            end
-        end
-    end
-    data.event.leader = tostring(event.leader or 'None') -- Leader?
-    data.event.type = title_case(event.type or 'Unknown')
-    data.event.name = title_case(event.name or 'Unknown')
-    data.event.date = ('%.2u\\%.2u\\%.4u'):format(date.month, date.day, date.year)
-    data.event.timestamp = tostring(timestamp)
-    report = data:to_xml()
-    file:append(report)
-    windower.add_to_chat(207, "Attendance saved as: "..filename)
-
-end
-
 function save_report_csv() 
     if (not is_ready) then
         windower.add_to_chat(207, "Just a little longer please.")
@@ -134,33 +70,28 @@ function save_report_csv()
     local current_members = get_player_data()
     local date = os.date('*t')
     local time = os.date('%H%M%S')
+    local time_c = os.date('%H:%M:%S')
     local name = windower.ffxi.get_player().name
     local timestamp = ('%.4u.%.2u.%.2u.%.2u'):format(date.year, date.month, date.day, time)
     local report = ""
 
-    local filename = (event.name ~= '' and (event.name ~= 'Unknown' and event.name) or 'attendance')..('_%.4u.%.2u.%.2u_%.2u.csv'):format(date.year, date.month, date.day, time)
+    local filename = 'attendance'..('_%.4u.%.2u.%.2u_%.2u.csv'):format(date.year, date.month, date.day, time)
 
     local file = files.new('/export/'..filename)
     if not file:exists() then
         file:create()
     end
+    --name,job/subjob,date,time,timezone,zonename
     for k,v in pairs(current_members) do
         if (not ignore_members:contains(v.name)) then
             report = report..v.name
-            report = report..","..title_case(event.name or '')
-            report = report..","..title_case(event.type or '')
-            report = report..","..('%.2u/%.2u/%.4u'):format(date.month, date.day, date.year)
-            report = report..",".."false" -- Early
-            report = report..",".."false" -- Ontime
-            report = report..",".."false" -- Late
-            report = report..","..tostring(event.leader and (title_case(event.leader) == v.name) or false) -- Leader?
-            report = report..","..tostring(T{"PLD","NIN","RUN"}:contains(v.main_job)) -- Tank?
-            report = report..","..tostring(T{"WHM"}:contains(v.main_job)) -- Healer?
-            report = report..","..tostring(T{"RDM","BRD","COR","GEO"}:contains(v.main_job)) -- Support?
-            report = report..","..tostring(T{"WAR","MNK","THF","DRK","BST","RNG","SAM","DRG","BLU","PUP","DNC"}:contains(v.main_job)) -- Physical DD?
-            report = report..","..tostring(T{"BLM","SMN","SCH"}:contains(v.main_job)) -- Magical DD?
+            report = report .. "," .. (v.main_job or "---") .. (v.main_job_lvl or "") .. "/" .. (v.sub_job or "---") .. (v.sub_job_lvl or "")
+            report = report..","..('%.4u-%.2u-%.2u'):format(date.year, date.month, date.day)
+            report = report..","..time_c..",UTC"..os.date("%z")
+            report = report .. "," .. v.zone
             report = report.."\n"
         end
+        windower.add_to_chat(211,report)
     end
     file:append(report)
 
@@ -173,34 +104,14 @@ function show_report()
         return
     end
     local role = "Role: Unknown"
-    local report = "Event: "
-
-    report = report.. title_case(event.name or 'Unknown')
-    report = report.. title_case(event.type and (" ["..event.type.."]") or '')
-    report = report.. " - Leader: "..tostring(event.leader or 'None').."\n"
+    local report = ""
 
     local current_members = get_player_data()
     local lines = 0
     for k,v in pairs(current_members) do
-        if (not ignore_members:contains(v.name)) then
-            local role = "Role: Unknown"
-            report = report..v.name
-            if (v.main_job and v.main_job ~= "NON") then
-                report = report.." ("..v.main_job..v.main_job_lvl.."/"..v.sub_job..v.sub_job_lvl..")"
-            end
-
-            if (T{"PLD","NIN","RUN"}:contains(v.main_job)) then
-                role = 'Role: Tank'
-            elseif (T{"WHM","SCH"}:contains(v.main_job)) then
-                role = 'Role: Healer'
-            elseif (T{"RDM","BRD","COR","GEO"}:contains(v.main_job)) then
-                role = 'Role: Support'
-            elseif (T{"WAR","MNK","THF","DRK","BST","RNG","SAM","DRG","BLU","PUP","DNC"}:contains(v.main_job)) then
-                role = 'Role: Physical DD'
-            elseif (T{"BLM","SMN"}:contains(v.main_job)) then
-                role = 'Role: Magical DD'
-            end
-            report = report.." "..role.."\n"
+        windower.add_to_chat(207, current_members[k].name)
+        for key,value in pairs(v) do
+            windower.add_to_chat(211, key..":"..value)
         end
     end
 
@@ -212,7 +123,7 @@ function show_report()
 end
 
 windower.register_event('load', function()
-    windower.add_to_chat(207, "Please wait 3 second before attempting to process reports to ensure alliance is fully loaded.")
+    windower.add_to_chat(207, "Please wait 3 second before attempting to process attendance to ensure alliance is fully loaded.")
     coroutine.schedule(ready, 3)
 end)
 
@@ -243,33 +154,8 @@ windower.register_event('addon command', function(...)
     if (cmd == "report") then
         show_report()
         return
-    elseif (cmd == "csv") then
+    elseif (cmd == "csv" or cmd == "now") then
         save_report_csv()
-        return
-    elseif (cmd == "xml") then
-        save_report_xml()
-        return
-    elseif (T{"event","ev"}:contains(cmd)) then
-        local n = title_case(args:concat(" "))
-        if (event_names:contains(n)) then
-            event.name = n
-            windower.add_to_chat(207, "Event name set to "..n)
-        else
-            windower.add_to_chat(207, n.." is not a valid event name.")
-        end
-        return
-    elseif (T{"type","evtype"}:contains(cmd)) then
-        local n = title_case(args:concat(" "))
-        if (event_types:contains(n)) then
-            event.type = n
-            windower.add_to_chat(207, "Event type set to "..n)
-        else
-            windower.add_to_chat(207, n.." is not a valid event type.")
-        end
-        return
-    elseif (T{"leader","lead","ldr"}:contains(cmd)) then
-        event.leader = title_case(args:concat(" "))
-        windower.add_to_chat(207, "Event leader set to "..event.leader)
         return
     elseif(T{"ignore","ign","ig"}:contains(cmd)) then
         local member = title_case(args:concat(" "))
